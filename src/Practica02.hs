dfc9a185a9a313a08d1a7b36a13dec786fa4d65f
darikneydan-creator <darikneydan@ciencias.unam.mx>
2025-10-01 11:23:47 -0600

    Update Practica02.hs

--- src/Practica02.hs
+++ src/Practica02.hs
@@ -19,18 +19,46 @@ toBin n = if mod n 2 == 1 then pegar (toBin (div n 2)) [I] else pegar (toBin (di
 suma :: Binario -> Binario -> Binario
 suma b1 b2 = toBin (toDecimal b1 + toDecimal b2)
 
---LISTAS
+--LISTAS 
 
-palindromo :: [a] -> Bool
-palindromo = undefined
+--Funciones auxiliares
+concatena :: [a] -> [a] -> [a]
+concatena [] ys = ys
+concatena (x:xs) ys = (x:concatena xs ys)
+
+reversa :: [a] -> [a]
+reversa [] = []
+reversa (x:xs) = concatena (reversa xs) [x] --O bien reversa xs ++ [x]
+
+contiene :: (Eq a) => [a] -> a -> Bool
+contiene [] _ = False
+contiene (x:xs) y = if x == y then True else contiene xs y
+
+diferencia :: (Eq a) => [a] -> [a] -> [a]
+diferencia [] _ = []
+diferencia (x:xs) ys = if contiene (ys) x then diferencia xs ys else concatena ([x]) (diferencia xs ys)
+
+
+--FIN DE LAS FUNCIONES AUXILIARES 
+
+palindromo :: (Eq a)=>[a] -> Bool 
+palindromo []= True
+palindromo (xs) = if (xs) == reversa(xs) then True else False
+
+
 
 --Funcion principal que calcula la diferencia simetrica (Aquellos elementos que esten en la union pero no en la interseccion)
-diferenciaSimetrica :: [a] -> [a] -> [a]
-diferenciaSimetrica = undefined
+diferenciaSimetrica :: (Eq a) => [a] -> [a] -> [a]
+diferenciaSimetrica [] []= []
+diferenciaSimetrica (xs) [] = xs
+diferenciaSimetrica [] (ys) = ys
+diferenciaSimetrica (xs) (ys) = concatena (diferencia xs ys) (diferencia ys xs)
 
 --Conjunto potencia
 conjuntoPotencia :: [a] -> [[a]]
-conjuntoPotencia = undefined
+conjuntoPotencia []= [[]]
+conjuntoPotencia (x:xs) = concatena (conjuntoPotencia xs) [x:ys | ys <- conjuntoPotencia xs]
+
 
 
 --LISTAS DE LONGITUD PAR
